bf5e134ab787669b92347974f1e26e7473ecbb40
JoSuzuki <jonathan@suzuki.pro.br>
2018-06-10 00:10:37 -0300

    Refactor app

--- hackathonfrontend/src/App.tsx
+++ hackathonfrontend/src/App.tsx
@@ -1,15 +1,23 @@
 import * as React from 'react';
+import axios from 'axios';
 
-class App extends React.Component {
+class App extends React.Component <any, any> {
+  public state={textFromServer: 'carregando'};
   
-  // public componentDidMount() {
-  // }
+  public componentDidMount() {
+    axios.get('http://127.0.0.1:8000/getjson/')
+    .then(response => {
+      console.log(response);
+      this.setState({textFromServer: JSON.stringify(response)});
+    })
+    .catch(error => console.warn(error));
+  }
   
   public render() {
 
     return (
       <React.Fragment>
-        <div>starter</div>
+        <div>{this.state.textFromServer}</div>
       </React.Fragment>
     );
   }
